fcc723b22835581fb30edd133639603440324450
Rawan Mortaja <33564662+rawan-mortja@users.noreply.github.com>
2022-11-07 21:12:00 +0200

    Add files via upload

---
+++ files.php
@@ -0,0 +1,16 @@
+<HTML>
+<form method='post' action='' enctype='multipart/form-data'>
+    <input type="file" name="file[]" id="file" multiple>
+        <br><br>
+    <input type='submit' name='submit' value='Upload'>
+</form>
+</HTML>
+<?php
+if(isset($_POST['submit'])){
+ $countfiles = count($_FILES['file']['name']);
+ for($i=0 ; $i<$countfiles ; $i++){
+  $filename = $_FILES['file']['name'][$i];
+  $upload = move_uploaded_file($_FILES['file']['tmp_name'][$i],'uploadFiles/'.$filename);
+ }
+}
+?>
